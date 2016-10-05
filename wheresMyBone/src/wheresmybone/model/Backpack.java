1e489e6b95ba11d0274479e166fe208ab1282e2f
short <short@192.168.2.14>
2016-10-05 07:21:12 -0500

    10/5/16 Backpack Class added

---
+++ wheresMyBone/src/wheresmybone/model/Backpack.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wheresmybone.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author short
+ */
+public class Backpack implements Serializable{
+    
+    //class instance variables
+    private String itemName;
+
+    public Backpack() {
+    }
+    
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.itemName);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Backpack{" + "itemName=" + itemName + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Backpack other = (Backpack) obj;
+        if (!Objects.equals(this.itemName, other.itemName)) {
+            return false;
+        }
+        return true;
+    }
+  
+    
+}
